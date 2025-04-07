03dec29b5bb3a1f06f128ec1b49683376ef70655
Ehijie Ifada <ifadaehijie@gmail.com>
2025-04-07 10:47:59 +0100

    Update: modification

--- frontend/src/components/HeroBanner.jsx
+++ frontend/src/components/HeroBanner.jsx
@@ -12,7 +12,7 @@ const HeroBanner = () => {
         <h1 className="text-lg md:text-xl font-semibold">Beats Solo Wireless</h1>
         <h2 className="text-2xl md:text-3xl font-bold text-black">Summer Sale</h2>
         <Link
-          to="/products/67f2a440c9994ce402eb7a6d"
+          to="/products/67f3ab194235a05f11c2371b"
           className="inline-block mt-2 bg-yellow-500 text-black px-4 py-2 rounded-lg hover:bg-gray-200 transition"
         >
           Shop now
