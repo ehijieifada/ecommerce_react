7116755a6d8888b14b3d0020bc76efceb8b77166
Ehijie Ifada <ifadaehijie@gemail.com>
2026-09-26 05:36:40 -0400

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://blisstechiq.netlify.app'
+    'https://blisstechiq.netlify.app',
+    'https://blisstechiq.shop'
   ],
   credentials: true
 }));
